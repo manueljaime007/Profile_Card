3dafbf04f6e9a14efb9451029c84fc359f6d67ca
Manuel Jaime <manueljaime0020@gmail.com>
2025-07-11 16:16:23 +0100

    Atualizando o front

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,11 @@ function App() {
       <section className="wrapper__section">
         <article className="profile__card">
           <div className="like__btn">
-            <img src="assets/img/ui/like_regular.png" alt="like_ui_icon" width="30" />
+            <img src="img/ui/like_regular.png" alt="like_ui_icon" width="30" />
           </div>
           <div className="share__btn"></div>
           <div className="img">
-            <img src="assets/img/img-1.jpg" alt="user_profile_pic_1" width="300" height="400" />
+            <img src="img/img-1.jpg" alt="user_profile_pic_1" width="300" height="400" />
           </div>
           <div className="content">
             <div className="user__info">
@@ -28,12 +28,12 @@ function App() {
             <div className="user__stacks">
               <h3 className="subtitle">Stacks</h3>
               <div className="stacks__group">
-                <img src="assets/img/img_stacks/html_icon.png" alt="html_logo" width="40" title="" />
-                <img src="assets/img/img_stacks/css_icon.png" alt="css_logo" width="40" />
-                <img src="assets/img/img_stacks/js_icon.png" alt="js_logo" width="40" />
-                <img src="assets/img/img_stacks/py_icon.png" alt="python_logo" width="40" />
-                <img src="assets/img/img_stacks/node_icon.png" alt="nodejs_logo" width="35" />
-                <img src="assets/img/img_stacks/mysql_icon.png" alt="mysql_logo" width="40" />
+                <img src="img/img_stacks/html_icon.png" alt="html_logo" width="40" title="" />
+                <img src="img/img_stacks/css_icon.png" alt="css_logo" width="40" />
+                <img src="img/img_stacks/js_icon.png" alt="js_logo" width="40" />
+                <img src="img/img_stacks/py_icon.png" alt="python_logo" width="40" />
+                <img src="img/img_stacks/node_icon.png" alt="nodejs_logo" width="35" />
+                <img src="img/img_stacks/mysql_icon.png" alt="mysql_logo" width="40" />
               </div>
             </div>
             <div className="button">
